e1cf19087a6af5b2eff3739f551e4f0460dbe459
swae <xiaoyuan1314@me.com>
2018-01-28 21:53:37 +0800

    定义：Auth(权限)、Role(角色) 二个model

--- app/models.py
+++ app/models.py
@@ -108,3 +108,25 @@ class Moviecol(db.Model):
 
     def __repr__(self):
         return "<Moviecol %r>" % self.id
+
+# 权限
+class Auth(db.Model):
+    __tablename__ = "auth"
+    id = db.Column(db.Integer, primary_key=True) # 编号
+    name = db.Column(db.String(100), unique=True) # 名称
+    url = db.Column(db.String(255), unique=True) # 地址
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow) #  添加日期
+
+    def __repr__(self):
+        return "<Auth %r>" % self.name
+
+# 角色
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True) # 编号
+    name = db.Column(db.String(100), unique=True) # 名称
+    auths = db.Column(db.String(600)) # 权限列表
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow) # 角色入库添加时间
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
