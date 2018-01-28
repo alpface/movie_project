f18aec8576cbf0c9c668d634251776e9a7ff478c
swae <xiaoyuan1314@me.com>
2018-01-28 22:43:28 +0800

    搭建前台home页面

--- app/home/views.py
+++ app/home/views.py
@@ -6,7 +6,8 @@
 # @Software: PyCharm
 
 from . import home
+from flask import render_template
 
 @home.route('/')
 def index():
-    return "<h1 style='color:blue'>this is home</h1>"
+    return render_template('home/index.html')
