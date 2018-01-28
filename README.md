c3fe1086689414220cd7a139997ac55552ea5c36
swae <xiaoyuan1314@me.com>
2018-01-28 20:41:57 +0800

    初始化数据库及models

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Flask 搭建的视频网站学习记录
 
 使用蓝图构建项目目录
+
+#### 运行项目
+- 创建基于python3.6的虚拟环境`movie_venv`:
+```
+mkvirtualenv -p /usr/local/bin/python3 movie_venv
+```
+注意: `/usr/local/bin/python3`为python3的路径，如果不确定可以使用: which python3查找
+- 激活虚拟环境:`source ~/movie_venv`
+- 安装依赖包:
+```
+pip3 install flask
+pip3 install -i http://pypi.douban.com/simple --trusted-host pypi.douban.com flask-sqlalchemy
+
+```
+
+#### 创建数据库
+登录mysql
+```
+mysql -uroot -p
+```
+创建名称为movie的数据库
+```
+create database movie chartset=uft8;
+```
+
+
--- app/models.py
+++ app/models.py
@@ -4,3 +4,42 @@
 # @Email   : xiaoyuan1314@me.com
 # @File    : models.py
 # @Software: PyCharm
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:8889'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
+# 会员
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True) # 编号
+    name = db.Column(db.String(100), unique=True) # 昵称 unique 为True就是唯一
+    pwd = db.Column(db.String(100)) # 密码
+    email = db.Column(db.String(100), unique=True) # 邮箱
+    phone = db.Column(db.String(11), unique=True) # 手机号
+    info = db.Column(db.Text) # 个性简介
+    face = db.Column(db.String(255), unique=True) # 头像
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow) # 注册日期
+    uuid = db.Column(db.String(255), unique=True) # 唯一标识符
+    userlogs = db.relationship('Userlog',backref='user') # 会员日志外键关系关联
+
+    def __repr__(self):
+        return "<User %r>" % self.name
+
+# 会员登录
+class UserLog(db.Model):
+    __tablename__ = 'userlod'
+    id = db.Column(db.Integer, primary_key=True) # 日志编号
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # 所属会员 关联User的外键
+    ip = db.Column(db.String(100)) # 登录ip
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 登录时间
+
+    def __repr__(self):
+        return "<Userlog %r>" % self.id
