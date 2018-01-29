8e7c71e6d72fbbf469cb9d43896532484db9fd23
swae <xiaoyuan1314@me.com>
2018-01-29 22:41:57 +0800

    主页电影列表页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -8,10 +8,6 @@
 from . import home
 from flask import render_template, redirect, url_for
 
-@home.route('/')
-def index():
-    return render_template('home/index.html')
-
 # 登录
 @home.route('/login/')
 def login():
@@ -53,8 +49,15 @@ def loginlog():
 def moviecol():
     return render_template('home/moviecol.html')
 
+# 主页
+@home.route('/')
+def index():
+    return render_template('home/index.html')
 
-
+# 动画
+@home.route('/animation/')
+def animation():
+    return render_template('home/animation.html')
 
 
 
