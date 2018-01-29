640e4e3c5b51a0d389479092f2dad90c633e4d66
swae <xiaoyuan1314@me.com>
2018-01-29 22:11:24 +0800

    用户页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -27,3 +27,34 @@ def logout():
 @home.route('/register/')
 def register():
     return render_template('home/register.html')
+
+# 会员中心
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+# 修改密码
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+# 评论页面
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+# 登录日志
+@home.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+# 收藏页面
+@home.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
+
+
+
+
+
+
