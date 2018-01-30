fe674b25bb03e0a9c660aed50d09295c904a027f
swae <xiaoyuan1314@me.com>
2018-01-30 21:00:56 +0800

    修改密码页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -22,3 +22,8 @@ def login():
 @admin.route('/logout/')
 def logout():
     return redirect(url_for('admin.login'))
+
+# 修改密码页面
+@admin.route('/pwd/')
+def pwd():
+    return render_template('admin/pwd.html')
